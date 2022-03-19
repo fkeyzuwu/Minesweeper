5c64e8a9cbc0b8c9717b95ac6966bfe9bc2d3b2b
fkeyzuwu <fkeyzuwu@gmail.com>
2022-03-19 16:45:51 +0200

    fixed it a lil so its better
    need to save it in file tho soonTM
    buit i gotta do my homework so not today!! uwu :-1:

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 
 public static class GameData
 {
-    public static int rowCount = 9;
-    public static int colCount = 9;
-    public static int bombCount = 10;
-    public static int highscore = int.MaxValue;
+    public static BoardCombination boardCombination = new BoardCombination(9, 9, 10);
+    public static Dictionary<BoardCombination, int> highscores = new Dictionary<BoardCombination, int>();
+    public static int rowCount { get => boardCombination.rowCount; set => boardCombination.rowCount = value; }
+    public static int colCount { get => boardCombination.colCount; set => boardCombination.colCount = value; }
+    public static int bombCount { get => boardCombination.bombCount; set => boardCombination.bombCount = value; }
 
     public static void SubmitTime(int timer)
     {
-        if(timer < highscore)
+        if (highscores.ContainsKey(boardCombination))
         {
-            highscore = timer;
+            if (timer < highscores[boardCombination])
+            {
+                highscores[boardCombination] = timer;
+            }
+            else
+            {
+                //have a 10 array of highscores
+            }
         }
-        Debug.Log($"Highscore: {highscore}");
+        else
+        {
+            highscores.Add(boardCombination, timer);
+        }
+
+        Debug.Log($"Highscore for row:{rowCount} col:{colCount} bomb:{bombCount} => {highscores[boardCombination]}");
+    }
+
+    public struct BoardCombination
+    {
+        public int rowCount;
+        public int colCount;
+        public int bombCount;
+
+        public BoardCombination(int rowCount, int colCount, int bombCount)
+        {
+            this.rowCount = rowCount;
+            this.colCount = colCount;
+            this.bombCount = bombCount;
+        }
     }
 }
